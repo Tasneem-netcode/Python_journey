1a0dbea377137418aeb35c74f183c9add8d60215
Tasneem-netcode <tasneem78899@gmail.com>
2025-05-31 22:41:21 +0530

    FILE I/O

--- 13_FILE_HANDLING/practise_set.py
+++ 13_FILE_HANDLING/practise_set.py
@@ -1,7 +1,66 @@
-f =open("demo.txt")
-content =f.read()
-if("twinkle" in content):
-    print(" Word Twinkle is present   in this content ")
+# f =open("demo.txt")
+# content =f.read()
+# if("twinkle" in content):
+#     print(" Word Twinkle is present   in this content ")
+# else:
+#     print("Not present")
+# f.close()
+
+
+#2nd ques:
+# import random
+
+# def game():
+#     print("You are playing the game...")
+#     score= random.randint(1 , 80)
+#     #fetch the hiscore:
+#     with open("hiscore.txt") as f :
+#         hiscore = f.read()
+#         if(hiscore != ""):
+#             hiscore = int(hiscore)
+#         else:
+#             hiscore = 0 
+#     print(f"Your score is :{score}")
+#     if(score > hiscore):
+#         #write this hiscore to file
+#         with open("hiscore.txt", "w") as f:
+#             f.write(str(score))
+
+#     return score
+# game()
+
+#3rd ques:
+# def generateTables(n):
+#     table = ""
+#     for i in range(1, 11):
+#         table += f"{n}X{i}= {n*i}\n"
+    
+#     with open(f"tables/table_{n}", "w") as f:
+#         f.write(table)
+
+# for i in range(2 , 21):
+#     generateTables(i)
+
+#4th ques
+# words = ["pillow" , "curtains", "bed"]
+# with open("demo2.txt" , "r") as f :
+#     content = f.read()
+# for word in words:
+#     NEWcontent = content.replace(word , "#" *len(word))
+
+# with open("demo2.txt" , "w") as f:
+#     f.write(NEWcontent)
+
+#5 th ques
+with open('log.html') as f:
+    lines = f.readlines() #each line will be stored in form of list into lines
+
+lineno = 1 
+for line in lines:
+    if("python" in line):
+        print(f"Yes python is present in line {lineno}")
+        break
+    lineno +=1
+
 else:
-    print("Not present")
-f.close()
+    print("Python not present")
