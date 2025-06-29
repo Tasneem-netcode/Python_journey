fab8de53917fadd757705f3de0afdd5da3d9dd84
Tasneem-netcode <tasneem78899@gmail.com>
2025-06-30 00:42:18 +0530

    Pandas

--- Numpy_in_python/initialize_different_types_of_arrays.py
+++ Numpy_in_python/initialize_different_types_of_arrays.py
@@ -1,48 +1,54 @@
 import numpy as np
 #All zero matrix:
 a =np.zeros((2,4) , dtype= "int16")
-# a =np.zeros((2,4) )
+a =np.zeros((2,4) )
 print(a)
-print(a.dtype)
+# print(a.dtype)
 
-b=np.ones((4,4), dtype = "int32")
-print(b)
+# b=np.ones((4,4), dtype = "int32")
+# print(b)
 
-c= np.full((2,2), 33)
-print(c)
+# c= np.full((2,2), 33)
+# print(c)
 
 # #Any other number (full_like)
 d =np.full_like(a, 4)
 print(d)
+d =np.full_like(3, 4)
+print(d)
 
-e = np.random.rand(1,4)
-print(e)
+# e = np.random.rand(1,4)
+# print(e)
 
-f = np.random.randint(7, 10 , size =(3,3))
-print(f)
+# f = np.random.randint(7, 10 , size =(3,3))
+# print(f)
 
-#The identity matrix:
-g = np.identity(4)
-print(g)
+# #The identity matrix:
+# g = np.identity(4)
+# print(g)
 
-#Example:
-output = np.ones((5,5))
-print(output)
+# #Example:
+# output = np.ones((5,5))
+# print(output)
 
-z = np.zeros((3,3))
-z[1,1] = 9
-print(z)
+# z = np.zeros((3,3))
+# z[1,1] = 9
+# print(z)
 
 # output[1: 4, 1:4] = z 
-output[1: -1, 1:-1] = z 
-print(output)
+# output[1: -1, 1:-1] = z 
+# print(output)
 
 
-# Be careful while copying an arrays !!!
-a =np.array([1,2,3])
-b = a. copy()
+# # Be careful while copying an arrays !!!
+# a =np.array([1,2,3])
+# b = a. copy()
 
-b[0] =100
+# b[0] =100
 
-print(a)
-# print(b)
+# print(a)
+# # print(b)
+
+
+em = np.empty((2,2))
+print(em)
